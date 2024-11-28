f7426e5166033a047e85dbd99e7854b92578830e
Marina <marinaakter878@gmail.com>
2024-11-28 21:32:42 +0600

    removing unnecessary div from root component

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -4,13 +4,12 @@ import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
   return (
-    <div className="">
+    
       <div className="w-10/12 mx-auto">
         <Navbar></Navbar>
         <Outlet></Outlet>
         <Footer></Footer>
       </div>
-    </div>
   );
 };
 
